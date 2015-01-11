0117fa077a3859a11fc3f6bd03823e6e4af0f62a
manukn <manuknarayanan@gmail.com>
2015-01-11 18:39:31 +0530

    test

--- app/views/pages/dashboard2.blade.php
+++ app/views/pages/dashboard2.blade.php
@@ -31,7 +31,16 @@
 
         </div>
 
-       
+        <div class="row">
+            <div class="col-md-7 col-xs-7 no-padding">
+                <div class="tiles white text-center">
+                    <h2 class="semi-bold text-primary weather-widget-big-text no-margin p-t-35 p-b-10">16°</h2>
+                    <div class="tiles-title blend p-b-25">CURRENTLY</div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+        </div>
 
 
     </div>   <!-- END ROW -->
