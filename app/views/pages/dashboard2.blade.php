a84d76e931d659dea878473427ab96366e3e4ac3
manukn <manuknarayanan@gmail.com>
2015-01-11 18:41:59 +0530

    dashboard

--- app/views/pages/dashboard2.blade.php
+++ app/views/pages/dashboard2.blade.php
@@ -24,6 +24,34 @@
         <div class="col-md-6 col-vlg-4 col-sm-12">
             <div class="row ">
 
+                <!-- BEGIN ANIMATED TILE -->
+                <div class="col-md-3 col-sm-6 m-b-20" data-aspect-ratio="true">
+                    <div class="live-tile slide ha " data-speed="750" data-delay="6000" data-mode="carousel">
+                        <div class="slide-front ha tiles green ">
+                            <div class="overlayer bottom-left fullwidth">
+                                <div class="overlayer-wrapper">
+                                    <div class="tiles gradient-black p-l-20 p-r-20 p-b-20 p-t-20">
+                                        <h4 class="text-white semi-bold no-margin">RUN AWAY GO </h4>
+                                        <h5 class="text-white semi-bold ">Queen's favourite</h5>
+                                        <p class="text-white semi-bold no-margin"><i class="icon-custom-up "></i> Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="assets/img/others/11.png" alt="" class="image-responsive-width xs-image-responsive-width"> </div>
+                        <div class="slide-back ha tiles green">
+                            <div class="overlayer bottom-left fullwidth">
+                                <div class="overlayer-wrapper">
+                                    <div class="tiles gradient-black p-l-20 p-r-20 p-b-20 p-t-20">
+                                        <h5 class="text-white semi-bold ">King's favourite</h5>
+                                        <p class="text-white semi-bold no-margin"><i class="icon-custom-up "></i> Read More</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <img src="assets/img/others/cover.jpg" alt="" class="image-responsive-width xs-image-responsive-width">
+                        </div>
+                    </div>
+                </div>
+                <!-- END ANIMATED TILE -->
 
 
             </div>
