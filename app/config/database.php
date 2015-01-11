4c373d4663fb3f3f256bee8645236df3b00a31d4
manukn <manuknarayanan@gmail.com>
2015-01-11 16:21:55 +0530

    git

--- app/config/database.php
+++ app/config/database.php
@@ -66,7 +66,7 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'forges',
+			'database' => 'forge',
 			'username' => 'forge',
 			'password' => '',
 			'charset'  => 'utf8',
