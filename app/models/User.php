544b0e828f3178e28aecaecc3757d44b091b99df
manukn <manuknarayanan@gmail.com>
2015-01-13 02:40:11 +0530

    confide added

--- app/models/User.php
+++ app/models/User.php
@@ -4,11 +4,13 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Confide\ConfideUser;
+use Zizaco\Confide\ConfideUserInterface;
 use Zizaco\Entrust\HasRole;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait,HasRole;
+class User extends Eloquent implements ConfideUserInterface {
+	use ConfideUser;
+	use HasRole;
 
 	/**
 	 * The database table used by the model.
