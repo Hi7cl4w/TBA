de198581cca553394fd4e27f216225325f6a4bd9
manukn <manuknarayanan@gmail.com>
2015-01-13 00:00:31 +0530

    Revert "git ignore"
    This reverts commit 8c1a4048f7cb3daa7c0712b5341551bdb3983cd6.
    
    Conflicts:
    	.idea/workspace.xml

--- app/models/User.php
+++ app/models/User.php
@@ -5,14 +5,10 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Zizaco\Entrust\HasRole;
-use Zizaco\Confide\ConfideUser;
-use Zizaco\Confide\ConfideUserInterface;
 
-class User extends Eloquent implements UserInterface,ConfideUserInterface {
+class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use ConfideUser;
 	use UserTrait, RemindableTrait,HasRole;
-
 
 	/**
 	 * The database table used by the model.
