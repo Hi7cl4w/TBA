7e3249f36568d01130712d9d883eaf3c2cc1ec11
Manu K <manuknarayanan@gmail.com>
2015-04-25 23:52:13 +0530

    Update Ticket.php

--- app/models/Ticket.php
+++ app/models/Ticket.php
@@ -3,12 +3,12 @@ class Ticket extends Eloquent {
 
     protected $table = 'ticket';
     public function usercustomer(){
-        return $this->belongsTo('User','customer_id','id');
+        return $this->belongsTo('User','Customer_id','id');
     }
     public function userstaff(){
         return $this->belongsTo('User','Staff_id','id');
     }public function usercustomerd(){
-        return $this->belongsTo('Customer','Staff_id','id');
+        return $this->belongsTo('Customer','Customer_id','id');
     }public function userstaffd(){
         return $this->belongsTo('Staff','Staff_id','id');
     }
